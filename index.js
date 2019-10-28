b1f971e92a40bb7e5c949f1febd4b3283b58c43b
dfjung4254 <rmsghk4254@naver.com>
2019-10-28 20:23:35 +0900

    fcm-pusher finished, need test when google auth released

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Alarm = require('./alarmModel');
+var User = require('./userModel');
 var dbConnect = require('./dbConnect');
 var fcm_admin = require('./fcm_pusher');
 dbConnect('user_auth');
@@ -44,7 +45,8 @@ intervalFunc = async () => {
             targetHour = (targetHour + 1) % 24;
         }
 
-        /* find db and fcm push */
+        /* find db and get Token list */
+        /* TODO: 각 기기에 해당하는 id의 토큰에 각 메세지를 전송 */
         var query = {
             isAlarmOn: true,
             hour: curHours,
@@ -53,18 +55,63 @@ intervalFunc = async () => {
         var day_selected_query = "day_selected." + day;
         query[day_selected_query] = true;
 
-        var pushList = await Alarm.find(query).catch(err => {
-            console.log(err);
-        });
+        var pushAlarmList = await Alarm.find(query)
+            .catch(err => {
+                console.log(err);
+            });
 
-        // pushList.
+        console.log('pushAlarmList : ' + JSON.stringify(pushAlarmList));
+
+        var pushInfo = new Object();
+        var userIdArray = new Array();
+        for (var alarm of pushAlarmList) {
+            console.log('alarm : ' + JSON.stringify(alarm));
+            pushInfo[alarm.userId] = {
+                userId: alarm.userId,
+                title: alarm.title,
+                hour: alarm.hour,
+                minute: alarm.minute
+            }
+            userIdArray.push(alarm.userId);
+        }
+
+        console.log('pushInfo(noToken) : ' + JSON.stringify(pushInfo));
+
+        var pushUserList = await User.find({
+            userId: userIdArray
+        }).catch(err => {
+            console.log(err);
+        })
+
+        for (var user of pushUserList) {
+            pushInfo[user.userId].token = user.fcm_token;
+        }
 
         /* fcm push */
-        // var response = await fcm_admin.messaging()
-        //     .send().catch(err => {
-        //         /* err occured */
-        //         console.log(JSON.stringify(err));
-        //     })
+        console.log('pushInfo : ' + JSON.stringify(pushInfo));
+        for (var uid of userIdArray) {
+            var pushObj = pushInfo[uid];
+            var message = {
+                data: {
+                    type: 'alarm',
+                    title: pushObj.title,
+                    hour: pushObj.hour,
+                    minute: pushObj.minute
+                },
+                token: pushObj.token
+            };
+
+            /* Error Catch 해야 함 - token 이 없을 수도 있음. */
+            console.log('message : ' + JSON.stringify(message));
+            var response = await fcm_admin.messaging()
+                .send(message)
+                .catch(err => {
+                    console.log('Error sending message:', error);
+                });
+
+            console.log('Successfully sent message:', response);
+
+        }
 
     }
 
